b1de62f7e244464a6b897c44bbea0559b1947867
Shade <shade@shade.ga>
2023-06-24 18:10:41 +0400

    Initial Commit

--- PassWraith/Utilities/PasswordHelper.cs
+++ PassWraith/Utilities/PasswordHelper.cs
@@ -10,20 +10,14 @@ namespace PassWraith.Utilities
     {
         public static string EncryptPassword(string password)
         {
-            // Generate a random salt
             string salt = BCrypt.Net.BCrypt.GenerateSalt();
-
-            // Hash the password using the generated salt
             string hashedPassword = BCrypt.Net.BCrypt.HashPassword(password, salt);
-
             return hashedPassword;
         }
 
         public static bool VerifyPassword(string password, string hashedPassword)
         {
-            // Verify the provided password against the hashed password
             bool passwordMatches = BCrypt.Net.BCrypt.Verify(password, hashedPassword);
-
             return passwordMatches;
         }
     }
